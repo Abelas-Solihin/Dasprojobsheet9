1bcd9ef8abdff43e799e449932219f90f8e617c3
Abelas-Solihin <abelassolihin@gmail.com>
2024-10-31 22:54:33 +0700

    modifikasi

--- ArrayRataNilai02.java
+++ ArrayRataNilai02.java
@@ -1,20 +1,27 @@
-
 import java.util.Scanner;
+
 public class ArrayRataNilai02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
+        
+        int[] nilaiMhs = new int[jumlahMahasiswa];
         double total = 0, rata2;
-
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        int Lulus = 0;
+        for (int i = 0; i < jumlahMahasiswa; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             nilaiMhs[i] = sc.nextInt();
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        for (int i = 0; i < jumlahMahasiswa; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                Lulus++;
+            }
         }
-        rata2 = total / nilaiMhs.length;
+        rata2 = total / jumlahMahasiswa;
         System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Jumlah mahasiswa yang lulus = " + Lulus);
     }   
 }
