783218929b7d9270d15efbbbaecac56a09de296b
Abelas-Solihin <abelassolihin@gmail.com>
2024-10-31 21:47:54 +0700

    Jobsheet9-02 (P.1)

--- ArraynNlai02.java
+++ ArraynNlai02.java
@@ -1,17 +1,22 @@
+
+
 import java.util.Scanner;
+
 public class ArraynNlai02 {
     public static void main(String[] args) {
-
-        Scanner sc = new java.util.Scanner(System.in);
-        int[] nilaiAkhir = new int [10];
-        
-        for (int i = 0; i < 10 ; i++){
-        System.out.println("Masukkan nilai akhir ke-" + i + ":");
-        nilaiAkhir[i] = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + ": ");
+            nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++){
-            System.out.println("Niai akhir ke-" + i + "adalah" + nilaiAkhir [i]);
+        System.out.println("\nStatus Kelulusan Mahasiswa:");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i] + " - Lulus");
+            } else {
+                System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i] + " - Tidak Lulus");
+            }
         }
-        sc.close();
     }
 }
